1d4a8601ef4fe08c5fccefe8fcaba8675c6e15f9
Sirin Chankao <sirin.cha@student.mahidol.edu>
2021-03-09 14:03:10 +0700

    Created App Bar with conditional Logout button

---
+++ frontend/src/views/user/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <AppBar :authenticated=true />
+</template>
+
+<script>
+import AppBar from '@/components/AppBar';
+
+export default {
+name: "Login",
+  components: {
+  AppBar,
+  },
+  props: {
+  authenticated: Boolean,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
