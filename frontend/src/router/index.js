402a8ef72ee6bee9504b36b07b1a6a436875c68d
Sirin Chankao <sirin.cha@student.mahidol.edu>
2021-03-10 20:45:14 +0700

    Changed Login file location

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import Home from '../views/Home.vue'
-import Login from '../views/user/Login.vue'
+import Login from '../views/Login.vue'
 
 Vue.use(VueRouter)
 
