a0ad75ca79eda1f4141bed51bdc023dfd41c8905
Sirin Chankao <sirin.cha@student.mahidol.edu>
2021-03-10 20:53:33 +0700

    Created Issuer dashboard page

---
+++ frontend/src/views/issuer/Dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <AppBar :authenticated=true />
+  </div>
+</template>
+
+<script>
+import AppBar from '@/components/AppBar';
+
+export default {
+  name: "Dashboard",
+  components: {
+    AppBar,
+  },
+  props: {
+    authenticated: Boolean,
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
