a1986fdac2bb5e5988f30e278a52faf9fe7f5448
Sirin Chankao <sirin.cha@student.mahidol.edu>
2021-03-12 21:53:56 +0700

    Create tabs for certificate issue request

--- frontend/src/views/issuer/Dashboard.vue
+++ frontend/src/views/issuer/Dashboard.vue
@@ -2,82 +2,83 @@
   <div>
     <AppBar :authenticated=true />
     <v-content>
-      <DataTable>
-<!--        <v-simple-table class="text-left">-->
-<!--          <thead>-->
-<!--          <tr>-->
-<!--            <th>-->
-<!--              Name-->
-<!--            </th>-->
-<!--            <th>-->
-<!--              Issuer-->
-<!--            </th>-->
-<!--            <th colspan="2" class="text-center">-->
-<!--              Actions-->
-<!--            </th>-->
-<!--          </tr>-->
-<!--          </thead>-->
-<!--          <tbody>-->
-<!--          <tr-->
-<!--              v-for="item in certificates"-->
-<!--              :key="item.name"-->
-<!--          >-->
-<!--            <td>{{ item.name }}</td>-->
-<!--            <td>{{ item.issuer }}</td>-->
-<!--            <td class="text-center">-->
-<!--              <div v-if="item.signed">-->
-<!--                Signed-->
-<!--              </div>-->
-<!--              <div v-else>-->
-<!--                <v-btn>Request Signing</v-btn>-->
-<!--              </div>-->
-<!--            </td>-->
-<!--            <td class="text-center">-->
-<!--              <v-btn class="ma-0">Export</v-btn>-->
-<!--            </td>-->
-<!--          </tr>-->
-<!--          </tbody>-->
-<!--        </v-simple-table>-->
-      </DataTable>
+      <v-container>
+        <v-tabs v-model="tab" centered>
+          <v-tab>Unissued Certificates</v-tab>
+          <v-tab>Issued Certificates</v-tab>
+        </v-tabs>
+        <v-tabs-items v-model="tab">
+          <v-tab-item>
+            <v-container>
+              <v-data-table
+                  v-model="selected"
+                  :headers="headers"
+                  :items="certificates"
+                  :items-per-page="5"
+                  show-select
+                  :single-select=false
+                  item-key="name"
+                  class="elevation-1 text-left">
+              </v-data-table>
+            </v-container>
+          </v-tab-item>
+          <v-tab-item>
+            <v-container>
+              <v-data-table
+                  :headers="headers"
+                  :items="certificates"
+                  :items-per-page="5"
+                  class="elevation-1 text-left">
+              </v-data-table>
+            </v-container>
+          </v-tab-item>
+        </v-tabs-items>
+      </v-container>
     </v-content>
   </div>
 </template>
 
 <script>
 import AppBar from '@/components/AppBar';
-import DataTable from "@/components/DataTable";
+// import CertificateRequestTab from "@/views/issuer/CertificateRequestTab";
 
 export default {
   name: "Dashboard",
   components: {
     AppBar,
-    DataTable,
+    // CertificateRequestTab
   },
   props: {
     authenticated: Boolean,
   },
   data () {
     return {
+      tab: null,
+      selected: [],
+      headers: [
+        {
+          text: 'Certificate',
+          align: 'start',
+          value: 'name',
+        },
+        { text: 'Issuer', value: 'issuer' },
+      ],
       certificates: [
         {
           name: 'A',
           issuer: 'MUIC',
-          signed: false,
         },
         {
           name: 'B',
           issuer: 'MUIC',
-          signed: true,
         },
         {
           name: 'C',
           issuer: 'MUIDS',
-          signed: false,
         },
         {
           name: 'D',
           issuer: 'MUIDS',
-          signed: false,
         },
       ],
     }
