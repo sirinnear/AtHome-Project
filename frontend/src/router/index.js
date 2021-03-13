fd7832d55dbfd952e86ca6da0f237f079eb73ece
Sirin Chankao <sirin.cha@student.mahidol.edu>
2021-03-13 23:16:22 +0700

    Renamed CertificateTables to IssuerCertificateTable

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,7 +3,7 @@ import VueRouter from 'vue-router'
 
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
-import IssuerCert from '../views/issuer/CertificateTables.vue'
+import IssuerCert from '../views/issuer/IssuerCertificateTable.vue'
 import IssuerTransfer from '../views/issuer/TransferRequests.vue'
 import AdminCert from '../views/admin/IssueCertificateRequests'
 import UserCert from '../views/user/CertificateTable'
