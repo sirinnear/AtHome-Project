b9bc15e3b75c158bec431c95f148f319f76548b8
Sirin Chankao <sirin.cha@student.mahidol.edu>
2021-03-13 16:32:14 +0700

    Added userID into mock data for unique item selection

--- frontend/src/views/issuer/TransferRequests.vue
+++ frontend/src/views/issuer/TransferRequests.vue
@@ -10,7 +10,7 @@
             :items-per-page="5"
             show-select
             :single-select=false
-            item-key="user"
+            item-key="userID"
             class="elevation-1 text-left">
           <template v-slot:top>
             <v-toolbar flat dense>
@@ -55,34 +55,42 @@ export default {
       requests: [
         {
           user: 'A',
+          userID: 1,
           organization: 'MUIC',
         },
         {
           user: 'B',
+          userID: 2,
           organization: 'MUIC',
         },
         {
           user: 'C',
+          userID: 3,
           organization: 'MUIDS',
         },
         {
           user: 'D',
+          userID: 4,
           organization: 'MUIDS',
         },
         {
           user: 'A',
+          userID: 5,
           organization: 'MUIC',
         },
         {
           user: 'B',
+          userID: 6,
           organization: 'MUIC',
         },
         {
           user: 'C',
+          userID: 7,
           organization: 'MUIDS',
         },
         {
           user: 'D',
+          userID: 8,
           organization: 'MUIDS',
         },
       ],
