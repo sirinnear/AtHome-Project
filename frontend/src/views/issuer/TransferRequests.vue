f08ee0f85d6f8da8c393f9bb9da8716c9760fb62
Sirin Chankao <sirin.cha@student.mahidol.edu>
2021-03-13 16:23:08 +0700

    Finished issuer transfer request page

--- frontend/src/views/issuer/TransferRequests.vue
+++ frontend/src/views/issuer/TransferRequests.vue
@@ -3,18 +3,28 @@
     <AppBar :authenticated=true />
     <v-content>
       <v-container>
-        <v-tabs v-model="tab" centered>
-          <v-tab>Unissued Certificates</v-tab>
-          <v-tab>Issued Certificates</v-tab>
-        </v-tabs>
-        <v-tabs-items v-model="tab">
-          <v-tab-item>
-            <UnissuedCertTab />
-          </v-tab-item>
-          <v-tab-item>
-            <IssuedCertTab />
-          </v-tab-item>
-        </v-tabs-items>
+        <v-data-table
+            v-model="selected"
+            :headers="headers"
+            :items="requests"
+            :items-per-page="5"
+            show-select
+            :single-select=false
+            item-key="user"
+            class="elevation-1 text-left">
+          <template v-slot:top>
+            <v-toolbar flat dense>
+              <v-btn text dense color="#ea292f" v-if="selected.length > 0">
+                <v-icon
+                    left
+                >
+                  mdi-check
+                </v-icon>
+                Approve Selected
+              </v-btn>
+            </v-toolbar>
+          </template>
+        </v-data-table>
       </v-container>
     </v-content>
   </div>
@@ -22,24 +32,62 @@
 
 <script>
 import AppBar from '@/components/AppBar';
-import UnissuedCertTab from "@/views/issuer/CertificateRequestTabs/UnissuedCertTab";
-import IssuedCertTab from "@/views/issuer/CertificateRequestTabs/IssuedCertTab";
 
 export default {
   name: "TransferRequests",
   components: {
-    IssuedCertTab,
     AppBar,
-    UnissuedCertTab
   },
   props: {
     authenticated: Boolean,
   },
   data () {
     return {
-      tab: null,
+      selected: [],
+      headers: [
+        {
+          text: 'User',
+          align: 'start',
+          value: 'user',
+        },
+        {text: 'Organization', value: 'organization'},
+      ],
+      requests: [
+        {
+          user: 'A',
+          organization: 'MUIC',
+        },
+        {
+          user: 'B',
+          organization: 'MUIC',
+        },
+        {
+          user: 'C',
+          organization: 'MUIDS',
+        },
+        {
+          user: 'D',
+          organization: 'MUIDS',
+        },
+        {
+          user: 'A',
+          organization: 'MUIC',
+        },
+        {
+          user: 'B',
+          organization: 'MUIC',
+        },
+        {
+          user: 'C',
+          organization: 'MUIDS',
+        },
+        {
+          user: 'D',
+          organization: 'MUIDS',
+        },
+      ],
     }
-  },
+  }
 }
 </script>
 
